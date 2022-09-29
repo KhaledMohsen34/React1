ac2cc42990ffa029364d74ca0aa77a4ff97aec64
KhaledMohsen34 <114262319+KhaledMohsen34@users.noreply.github.com>
2022-09-29 11:48:23 +0200

    first commit

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import "./styles.css";
+
+export default function ProductDetails() {
+
+    let { id } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
+    const [allProducts, setAllProducts] = useState([]);
+
+    var url = "https://fakestoreapi.com/products/" + id;
+
+    useEffect(() => {
+        fetch(url)
+            .then(result => result.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setAllProducts(result);
+                    setIsLoading(false);
+                }
+            )
+    }, [isLoading]);
+
+
+    return (
+
+        <div className='finalPro'>
+            {/*  write new component here for display product details */}
+            <div className='finalTxt'>
+                <h1>Details of product {id}</h1>
+                <div>title : {allProducts.title}</div>
+                <div>description : {allProducts.description}</div>
+                <div>price : {allProducts.price}</div>
+                <div>category : {allProducts.category}</div>
+            </div>
+
+            <div className='finalImg' >
+                <img src={allProducts.image} style={{ width: "100px" }} />
+            </div>
+        </div>
+    )
+}
